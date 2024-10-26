5bb04dbab2837fed74079c61ace8c308834b1e4d
Alan Vega <a01750658@tec.mx>
2024-10-25 19:26:50 -0600

    interfaz para vertex

---
+++ app/src/main/java/mx/sct/tienditaya/model/VertexAiService.kt
@@ -0,0 +1,12 @@
+package mx.sct.tienditaya.model
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface VertexAIService {
+    @Headers("Content-Type: application/json")
+    @POST("v1/projects/{project-id}/locations/{location}/endpoints/{endpoint-id}:predict")
+    fun sendPrompt(@Body requestBody: RequestBody): Call<ResponseBody>
+}
