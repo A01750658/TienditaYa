eab79adb01b13b6bc597bcd8285ad96d5cd03608
Cesar Flores <A01751101@tec.mx>
2024-10-26 01:35:42 -0600

    Borrar greeting ahora sí

--- app/src/main/java/mx/sct/tienditaya/view/Main.kt
+++ app/src/main/java/mx/sct/tienditaya/view/Main.kt
@@ -31,7 +31,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import mx.sct.tienditaya.Greeting
 import mx.sct.tienditaya.R
 import mx.sct.tienditaya.ui.theme.TienditaYaTheme
 
